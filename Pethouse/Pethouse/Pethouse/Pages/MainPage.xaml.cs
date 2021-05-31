c833e49c051ecdc297e1332ff27f7ce197e1c82c
sami kling <sakling@live.com>
2021-05-31 21:49:30 +0300

    PET-16 Edited main view to show pets photo.

--- Pethouse/Pethouse/Pethouse/Pages/MainPage.xaml.cs
+++ Pethouse/Pethouse/Pethouse/Pages/MainPage.xaml.cs
@@ -3,6 +3,7 @@ using Pethouse.Models;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Net.Http;
 using Xamarin.Forms;
@@ -41,45 +42,70 @@ namespace Pethouse
             YourPets = new List<Pets>();
             //Pets pets = new Pets();
             HttpClient client = new HttpClient(); //Metodin alustus, jolla yhdistetään API:n
-            client.BaseAddress = new Uri("https://pethouse.azurewebsites.net/"); //Kurssilla aiemmin luodun API:n osoite Azuressa
-            string dogjson = await client.GetStringAsync("api/pets/user/dogs/"+LoginInfo.UserId);// +LoginInfo.UserId); //Lähetetään GET pyyntö API:lle
-            try
-            {
-                //var pets = JsonConvert.DeserializeObject<List<Pets>>(json);
-                string[] dogs = JsonConvert.DeserializeObject<string[]>(dogjson);
-                dogListView.ItemsSource = dogs;
-            }
-            catch (Exception ex)
-            {
+            client.BaseAddress = new Uri("https://pethouse.azurewebsites.net/");
+            string json = await client.GetStringAsync("/api/pets/"+LoginInfo.UserId);
 
-                Debug.WriteLine(@"ERROR{0}", ex);
-            }
-            string catjson = await client.GetStringAsync("api/pets/user/cats/"+LoginInfo.UserId);// +LoginInfo.UserId); //Lähetetään GET pyyntö API:lle
-            try
-            {
-                //var pets = JsonConvert.DeserializeObject<List<Pets>>(json);
-                string[] cats = JsonConvert.DeserializeObject<string[]>(catjson);
-                catListView.ItemsSource = cats;
-            }
-            catch (Exception ex)
-            {
+            IEnumerable<Pets> pets = JsonConvert.DeserializeObject<Pets[]>(json);
+            ObservableCollection<Pets> dataa = new ObservableCollection<Pets>(pets);
+            petsList.ItemsSource = dataa;
 
-                Debug.WriteLine(@"ERROR{0}", ex);
-            }
-            string json = await client.GetStringAsync("api/pets/user/test/" + LoginInfo.UserId);// +LoginInfo.UserId); //Lähetetään GET pyyntö API:lle
-            try
+            //If petlist is empty and user has no pets --- Button to add new pets
+            if (dataa == null)
             {
-                //var pets = JsonConvert.DeserializeObject<List<Pets>>(json);
-                IEnumerable<Pets> pets = JsonConvert.DeserializeObject<IEnumerable<Pets>>(json);
-                catListView.ItemsSource = pets;
+                //do something
             }
-            catch (Exception ex)
-            {
+            
+        }
 
-                Debug.WriteLine(@"ERROR{0}", ex);
-            }
-
-
+        private void petsList_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            //New Info page --> add Meds, vacs and whatnot. --> Edit pet info
         }
     }
+   
 }
+
+
+
+
+
+
+            //client.BaseAddress = new Uri("https://pethouse.azurewebsites.net/"); //Kurssilla aiemmin luodun API:n osoite Azuressa
+            //string dogjson = await client.GetStringAsync("api/pets/user/dogs/"+LoginInfo.UserId);// +LoginInfo.UserId); //Lähetetään GET pyyntö API:lle
+            //try
+            //{
+            //    //var pets = JsonConvert.DeserializeObject<List<Pets>>(json);
+            //    string[] dogs = JsonConvert.DeserializeObject<string[]>(dogjson);
+            //    dogListView.ItemsSource = dogs;
+            //}
+            //catch (Exception ex)
+            //{
+
+            //    Debug.WriteLine(@"ERROR{0}", ex);
+            //}
+            //string catjson = await client.GetStringAsync("api/pets/user/cats/"+LoginInfo.UserId);// +LoginInfo.UserId); //Lähetetään GET pyyntö API:lle
+            //try
+            //{
+            //    //var pets = JsonConvert.DeserializeObject<List<Pets>>(json);
+            //    string[] cats = JsonConvert.DeserializeObject<string[]>(catjson);
+            //    catListView.ItemsSource = cats;
+            //}
+            //catch (Exception ex)
+            //{
+
+            //    Debug.WriteLine(@"ERROR{0}", ex);
+            //}
+            //string json = await client.GetStringAsync("api/pets/user/test/" + LoginInfo.UserId);// +LoginInfo.UserId); //Lähetetään GET pyyntö API:lle
+            //try
+            //{
+            //    //var pets = JsonConvert.DeserializeObject<List<Pets>>(json);
+            //    IEnumerable<Pets> pets = JsonConvert.DeserializeObject<IEnumerable<Pets>>(json);
+            //    catListView.ItemsSource = pets;
+            //}
+            //catch (Exception ex)
+            //{
+
+            //    Debug.WriteLine(@"ERROR{0}", ex);
+            //}
+
+
