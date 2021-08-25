d1f96418d26e909852f15f3a26933362b5031436
Sami Kling <52190537+samikling@users.noreply.github.com>
2021-08-25 20:08:07 +0300

    PET-30 Take user input from UI and save to variables
    Variables now get taken from UI and saved as an Object, that will be sent to the API

--- Pethouse/Pethouse/Pethouse/Pages/AddPetPage.xaml.cs
+++ Pethouse/Pethouse/Pethouse/Pages/AddPetPage.xaml.cs
@@ -1,6 +1,12 @@
 ﻿
+using Newtonsoft.Json;
+using System;
+using System.Net.Http;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Pethouse.Models;
+using System.Collections.Generic;
+using Xamarin.Forms.Internals;
 
 namespace Pethouse.Pages
 {
@@ -20,9 +26,112 @@ namespace Pethouse.Pages
     Breed_id - Based on race selection show all breeds for that race
     */
     {
+        private List<Races> races = new List<Races>();
+        private List<Breeds> breedsList = new List<Breeds>();
         public AddPetPage()
         {
             InitializeComponent();
+            LoadRaces();
+
+
+
+
+
+        }
+        public async void LoadRaces()
+        {
+            
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://pethouse.azurewebsites.net/");
+            string json = await client.GetStringAsync("/api/races/");
+            races = JsonConvert.DeserializeObject<List<Races>>(json);
+            
+            List<string> raceList = new List<string>();
+            foreach(Races race in races)
+            {
+                raceList.Add(race.Racename);
+            }
+            racePicker.ItemsSource = raceList;
+        }
+        public async void LoadBreeds(object sender, EventArgs e)
+        {
+            
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://pethouse.azurewebsites.net/");
+            string json = await client.GetStringAsync("/api/breeds/");
+            breedsList = JsonConvert.DeserializeObject<List<Breeds>>(json);
+      
+            List<String> breed = new List<String>();
+            foreach(Breeds breeds in breedsList)
+            {
+                if (racePicker.SelectedItem.Equals("Dog") || racePicker.SelectedItem.Equals(1))
+                {
+                    if (breeds.RaceId == 1)
+                    {
+                        breed.Add(breeds.Breedname);
+                    }
+
+                }
+                else if (racePicker.SelectedItem.Equals("Cat") || racePicker.SelectedItem.Equals(2))
+                {
+
+                    if (breeds.RaceId == 2)
+                    {
+                        breed.Add(breeds.Breedname);
+                    }
+                }
+            }
+            breedPicker.ItemsSource = breed;
+          
+
+        }
+        /*
+         *--------------------------------------------------------------------------
+         * 25.08.2021-Lisätty toiminto joka hakee Pickkereiden indeksin perusteella
+         * BreedId ja RaceId tiedon. Lisätty Debug tekstikenttä. Lisätty ADD nappi.
+         * Lisätty Rodun hakutoiminto.
+         * -------------------------------------------------------------------------
+         * TODO:
+         * -------------------------------------------------------------------------
+         * Seuraavaksi selvitys, miten tiedot viedään kantaan, nyt kun kaikki tar-
+         * vittavat tiedot saadaan poimittua käyttöliittymästä. Pitää myös selvit-
+         * tää mitä tehdään valokuva toiminnon kanssa.
+         * -------------------------------------------------------------------------
+         */
+        public async void AddPet(object sender, EventArgs e)
+        {
+            int breedIdmem = 0;
+            int raceIdmem = 0;
+            raceIdmem = races[racePicker.SelectedIndex].RaceId;
+            breedIdmem = breedsList[breedPicker.SelectedIndex].BreedId;
+            
+            debugEntry.Text = LoginInfo.UserId.ToString()
+                + nameEntry.Text + bdatePicker.Date.ToString() +
+                raceIdmem.ToString() + breedIdmem.ToString();
+
+
+            Pets pets = new Pets()
+            {
+                UserId = LoginInfo.UserId,
+                Petname = nameEntry.Text,
+                Birthdate = bdatePicker.Date,
+                Photo = photoButton.Text,
+                RaceId = raceIdmem,
+                BreedId = breedIdmem
+            };
+        }
+
+        private void racePicker_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (racePicker.SelectedItem != null)
+            {
+                breedPicker.IsEnabled = true;
+                LoadBreeds(null, null);
+            }
+            else
+            {
+                breedPicker.IsEnabled = false;
+            }
         }
     }
 }
